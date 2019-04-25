7958a074d814e1969f09e5737fa3ac69168d8556
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2019-04-25 20:56:53 -0300

    extract the theme into his own independent file, and, then, import it into App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from './theme';
 
 import { Header, Hero, Footer, MyProjects } from './layout/';
 import Portfolio from './Portfolio';
@@ -28,43 +29,11 @@ const theme = createMuiTheme({
 {
   /* @palette https://material.io/tools/color/#!/?view.left=1&view.right=0&primary.color=2d5e72&secondary.color=0d3645 */
 }
-const theme = createMuiTheme({
-  palette: {
-    common: { black: 'rgba(0, 0, 0, 1)', white: 'rgba(255, 255, 255, 1)' },
-    background: {
-      paper: '#2d5f72',
-      default: '#2d5f72'
-    },
-    primary: {
-      light: 'rgba(92, 139, 161, 1)',
-      main: '#75c9d2',
-      dark: 'rgba(0, 52, 71, 1)',
-      contrastText: 'rgba(255, 255, 255, 1)'
-    },
-    secondary: {
-      light: 'rgba(60, 96, 112, 1)',
-      main: 'rgba(13, 54, 69, 1)',
-      dark: 'rgba(0, 16, 30, 1)',
-      contrastText: 'rgba(255, 255, 255, 1)'
-    },
-    error: {
-      light: '#e57373',
-      main: '#f44336',
-      dark: '#d32f2f',
-      contrastText: 'rgba(214, 214, 214, 1)'
-    },
-    text: {
-      primary: 'rgba(255, 255, 255, 1)',
-      secondary: 'rgba(0, 0, 0, 0.54)',
-      disabled: 'rgba(0, 0, 0, 0.38)',
-      hint: 'rgba(0, 0, 0, 0.38)'
-    }
-  }
-});
 console.log(theme);
+const mainTheme = createMuiTheme(theme);
 function App() {
   return (
-    <MuiThemeProvider theme={theme}>
+    <MuiThemeProvider theme={mainTheme}>
       <React.Fragment>
         <CssBaseline />
         <Header />
