f58a8ece36756919a889e511e1322c9353cc9d70
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2019-06-21 03:17:04 -0300

    Updates

---
+++ precache-manifest.e6cf268a5c6cfc9b77afd0ca9f7cf9c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bf1b0d5900481972e9c",
+    "url": "/ricardobossan/portfolio2/static/js/runtime~main.344de578.js"
+  },
+  {
+    "revision": "537dfa894036b64ec114",
+    "url": "/ricardobossan/portfolio2/static/js/main.6133889e.chunk.js"
+  },
+  {
+    "revision": "9e99770de4e3c82059f0",
+    "url": "/ricardobossan/portfolio2/static/js/2.0e9eca0b.chunk.js"
+  },
+  {
+    "revision": "537dfa894036b64ec114",
+    "url": "/ricardobossan/portfolio2/static/css/main.b39a4cfe.chunk.css"
+  },
+  {
+    "revision": "36e97787f5be63a291eb5281e475acca",
+    "url": "/ricardobossan/portfolio2/index.html"
+  }
+];
